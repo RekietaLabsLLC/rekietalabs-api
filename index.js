fccc45ffe0dd89c7d7fb0da72cd40033134131d4
RekietaLabs <kmrekieta@gmail.com>
2025-06-09 17:34:10 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// index.js
 import express from 'express';
 import signupRouter from './functions/signup.js';
 import loginRouter from './functions/login.js';
@@ -8,11 +9,11 @@ const PORT = process.env.PORT || 10000;
 // Middleware to parse JSON bodies
 app.use(express.json());
 
-// Use your routes
+// Routes
 app.use('/signup', signupRouter);
 app.use('/login', loginRouter);
 
-// Simple health check route
+// Health check route
 app.get('/', (req, res) => {
   res.send('API is running!');
 });
