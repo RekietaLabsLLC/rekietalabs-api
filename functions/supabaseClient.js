31420c50b9dbba48a9d5ab32d3967ab6fc6a7f6d
RekietaLabs <kmrekieta@gmail.com>
2025-06-14 16:22:45 -0500

    Update supabaseClient.js

--- functions/supabaseClient.js
+++ functions/supabaseClient.js
@@ -1,6 +1,7 @@
+// functions/supabaseClient.js
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.SUPABASE_URL;
-const supabaseAnonKey = process.env.SUPABASE_ANON_KEY;
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+export const supabase = createClient(
+  process.env.SUPABASE_URL,
+  process.env.SUPABASE_SERVICE_ROLE_KEY
+);
