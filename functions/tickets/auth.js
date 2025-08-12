6eb7a38125af014f147096c641a0b9a7ea3cd78c
RekietaLabs <kmrekieta@gmail.com>
2025-08-12 13:58:22 -0500

    Create auth.js

---
+++ functions/tickets/auth.js
@@ -0,0 +1,13 @@
+// functions/tickets/auth.js
+
+export const admins = [
+  { username: 'kellan@rekietalabs.com', password: 'URMOM@123' },
+  { username: 'jmrekieta@yahoo.com', password: 'Beauty1!' },
+  // Add more admin users here
+];
+
+export const staff = [
+  { username: 'staff1', password: 'staffpass1' },
+  { username: 'staff2', password: 'staffpass2' },
+  // Add more staff users here
+];
